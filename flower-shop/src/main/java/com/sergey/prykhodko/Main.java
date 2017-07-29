c6d7afe7bfa5dd661132113329576426cf16c38f
jimmy <sergii.pryhodko@gmail.com>
2017-07-29 06:37:23 +0300

    bouquet adding

--- flower-shop/src/main/java/com/sergey/prykhodko/Main.java
+++ flower-shop/src/main/java/com/sergey/prykhodko/Main.java
@@ -36,6 +36,9 @@ public class Main
                     break;
                 case "add flowers":
                     mainController.addFlowersToStock(in);
+                case "create bouquet":
+                    mainController.createBouquet(in);
+                    
 
             }
         }while (!command.equalsIgnoreCase("exit"));
